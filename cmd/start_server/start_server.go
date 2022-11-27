1b554626b25fbcd25fcb817ad9b6c6c0e9f89fa4
Abraham TEWA <4184985+AbrahamTewa@users.noreply.github.com>
2022-11-27 11:37:57 +0100

    feat(bin/start_server): `--stdio` parameter replaced by `stdio` command

--- cmd/start_server/start_server.go
+++ cmd/start_server/start_server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 
@@ -8,15 +9,20 @@ import (
 )
 
 func main() {
-	if len(os.Args) > 1 {
-		switch os.Args[1] {
-		case "--version":
-			fmt.Println(lsp.GetServerVersion())
-			return
-		case "--stdio":
-			lsp.StartServerStdio()
-			return
-		}
+	versionRef := flag.Bool("version", false, "display version")
+	flag.Parse()
+
+	// Parameter: version
+	version := *versionRef
+	if version {
+		fmt.Println(lsp.GetServerVersion())
+		return
+	}
+
+	// Command: stdio
+	if len(os.Args) > 1 && os.Args[1] == "stdio" {
+		lsp.StartServerStdio()
+		return
 	}
 
 	lsp.StartServer()
