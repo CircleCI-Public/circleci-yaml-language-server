0008c6afeef95d18881266a2e9683e8f06d7db2d
abdelDriowya <abdel.driowya@circleci.com>
2022-11-30 10:31:01 +0100

    move logic inside setToken command if

--- pkg/server/methods/executeCommand.go
+++ pkg/server/methods/executeCommand.go
@@ -13,13 +13,13 @@ func (methods *Methods) ExecuteCommand(reply jsonrpc2.Replier, req jsonrpc2.Requ
 	if err := json.Unmarshal(req.Params(), &params); err != nil {
 		return reply(methods.Ctx, nil, fmt.Errorf("%s: %w", jsonrpc2.ErrParse, err))
 	}
-	token := params.Arguments[0].(string)
-	if methods.Cache.TokenCache.GetToken() != token {
-		methods.Cache.OrbCache.RemoveOrbs()
-		methods.Cache.RemoveOrbFiles()
-	}
-	methods.Cache.TokenCache.SetToken(token)
 	if params.Command == "setToken" {
+		token := params.Arguments[0].(string)
+		if methods.Cache.TokenCache.GetToken() != token {
+			methods.Cache.OrbCache.RemoveOrbs()
+			methods.Cache.RemoveOrbFiles()
+		}
+		methods.Cache.TokenCache.SetToken(token)
 		filesCache := methods.Cache.FileCache.GetFiles()
 		for _, file := range filesCache {
 			go methods.notificationMethods(methods.Cache.FileCache, *file)
