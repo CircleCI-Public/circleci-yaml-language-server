96facba4a1eaac61422469e4d4059eb6a8c71e6a
Abraham TEWA <4184985+AbrahamTewa@users.noreply.github.com>
2022-12-01 10:21:42 +0100

    refactor: renaming `parser.GetParsedYAMLWithURI` to `parser.ParseFromURI`

--- pkg/services/definition_test.go
+++ pkg/services/definition_test.go
@@ -20,7 +20,7 @@ func TestDefinition(t *testing.T) {
 	os.Setenv("SCHEMA_LOCATION", schemaPath)
 	cache := utils.CreateCache()
 
-	parsedOrb, err := parser.GetParsedYAMLWithURI(uri.File(path.Join("./testdata/orb.yaml")))
+	parsedOrb, err := parser.ParseFromURI(uri.File(path.Join("./testdata/orb.yaml")))
 
 	if err != nil {
 		panic(err)
