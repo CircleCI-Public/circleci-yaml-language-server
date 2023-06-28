a23aff2a96503b76cc1277c7fb89e04ac08b46e5
Charles Francoise <charles.francoise@circleci.com>
2023-06-28 18:19:19 +0200

    fix completion test

--- pkg/services/complete_test.go
+++ pkg/services/complete_test.go
@@ -238,21 +238,12 @@ func TestComplete(t *testing.T) {
 				},
 			},
 			want: []protocol.CompletionItem{
-				{
-					Label: "medium",
-				},
-				{
-					Label: "macos.x86.medium.gen2",
-				},
-				{
-					Label: "large",
-				},
-				{
-					Label: "macos.x86.metal.gen1",
-				},
-				{
-					Label: "macos.m1.large.gen1",
-				},
+				{Label: "medium"},
+				{Label: "macos.x86.medium.gen2"},
+				{Label: "large"},
+				{Label: "macos.m1.medium.gen1"},
+				{Label: "macos.m1.large.gen1"},
+				{Label: "macos.x86.metal.gen1"},
 			},
 		},
 		{
