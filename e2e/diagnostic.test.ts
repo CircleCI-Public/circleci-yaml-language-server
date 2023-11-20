2ff9ab4551b3e1686817a13ba83549e81c1dcf89
Jeremy Vincent <jeremy.vincent@circleci.com>
2023-11-20 11:05:55 +0100

    style: reset formatting

--- e2e/diagnostic.test.ts
+++ e2e/diagnostic.test.ts
@@ -1,47 +1,47 @@
 import { commands } from './utils';
 
 describe('Diagnostic testing files', () => {
-    jest.setTimeout(10000);
+  jest.setTimeout(10000);
 
-    it('config1.yml', async () => {
-        const testingFile = 'config1.yml';
-        const diagnostics = await commands.didOpen(testingFile);
+  it('config1.yml', async () => {
+    const testingFile = 'config1.yml';
+    const diagnostics = await commands.didOpen(testingFile);
 
-        expect(diagnostics).toMatchSnapshot();
-    });
+    expect(diagnostics).toMatchSnapshot();
+  });
 
-    it('autocomplete-jobs.yml', async () => {
-        const testingFile = 'invalid-files/autocomplete-jobs.yml';
-        const diagnostics = await commands.didOpen(testingFile);
+  it('autocomplete-jobs.yml', async () => {
+    const testingFile = 'invalid-files/autocomplete-jobs.yml';
+    const diagnostics = await commands.didOpen(testingFile);
 
-        expect(diagnostics).toMatchSnapshot();
-    });
+    expect(diagnostics).toMatchSnapshot();
+  });
 
-    it('diagnostic.yml', async () => {
-        const testingFile = 'diagnostic.yml';
-        const diagnostics = await commands.didOpen(testingFile);
+  it('diagnostic.yml', async () => {
+    const testingFile = 'diagnostic.yml';
+    const diagnostics = await commands.didOpen(testingFile);
 
-        expect(diagnostics).toMatchSnapshot();
-    });
+    expect(diagnostics).toMatchSnapshot();
+  });
 
-    it('Matrix alias', async () => {
-        const testingFile = 'matrix-alias.yml';
-        const diagnostics = await commands.didOpen(testingFile);
+  it('Matrix alias', async () => {
+    const testingFile = 'matrix-alias.yml';
+    const diagnostics = await commands.didOpen(testingFile);
 
-        expect(diagnostics).toMatchSnapshot();
-    });
+    expect(diagnostics).toMatchSnapshot();
+  });
 
-    it('Inline parameter', async () => {
-        const testingFile = 'parameter-inline.yml';
-        const diagnostics = await commands.didOpen(testingFile);
+  it('Inline parameter', async () => {
+    const testingFile = 'parameter-inline.yml';
+    const diagnostics = await commands.didOpen(testingFile);
 
-        expect(diagnostics).toMatchSnapshot();
-    });
+    expect(diagnostics).toMatchSnapshot();
+  });
 
-    it('Undefined parameter', async () => {
-        const testingFile = 'invalid-files/not-defined-param.yml';
-        const diagnostics = await commands.didOpen(testingFile);
+  it('Undefined parameter', async () => {
+    const testingFile = 'invalid-files/not-defined-param.yml';
+    const diagnostics = await commands.didOpen(testingFile);
 
-        expect(diagnostics).toMatchSnapshot();
-    });
+    expect(diagnostics).toMatchSnapshot();
+  });
 });
