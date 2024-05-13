b97527c1d4430d577ec6b64340cb557ab8888b0f
abdelDriowya <111757332+abdelDriowya@users.noreply.github.com>
2024-05-13 15:30:29 +0200

    update ubuntu version

--- pkg/utils/machine_images.go
+++ pkg/utils/machine_images.go
@@ -10,6 +10,7 @@ var ValidARMOrMachineImagesUbuntu2004 = []string{
 	// Ubuntu 2004
 	"ubuntu-2004:current",
 	"ubuntu-2004:edge",
+	"ubuntu-2004:2024.04.4",
 	"ubuntu-2004:2024.01.2",
 	"ubuntu-2004:2024.01.1",
 	"ubuntu-2004:2023.10.1",
@@ -34,6 +35,10 @@ var ValidARMOrMachineImagesUbuntu2204 = []string{
 	// Ubuntu 2204
 	"ubuntu-2204:current",
 	"ubuntu-2204:edge",
+	"ubuntu-2204:2024.04.4",
+	"ubuntu-2204:2024.01.2",
+	"ubuntu-2204:2024.01.1",
+	"ubuntu-2204:2023.10.1",
 	"ubuntu-2204:2023.07.2",
 	"ubuntu-2204:2023.04.2",
 	"ubuntu-2204:2023.04.1",
