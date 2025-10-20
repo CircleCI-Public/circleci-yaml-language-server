9dc80f29545ff7dca55d3010401d7676132f35f9
Parker Rowe <parker.rowe@circleci.com>
2025-10-20 14:04:32 -0400

    feat: Consolidate schemas into just schema.json (#377)

--- editors/vscode/out/utils/devMode.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.isInDevMode = void 0;
-function isInDevMode() {
-    return process.env.CCI_DEV === 'true';
-}
-exports.isInDevMode = isInDevMode;
-//# sourceMappingURL=devMode.js.map
